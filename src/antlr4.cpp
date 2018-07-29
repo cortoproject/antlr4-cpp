3c7036843471a4687c19cc497f0362c73eadb1d7
Sander Mertens <sander.mertens8@gmail.com>
2018-07-29 15:46:25 -0700

    Build static libraries on Linux systems

--- src/antlr4.cpp
+++ src/antlr4.cpp
@@ -1,14 +1,9 @@
 
 #include <antlr4/cpp/cpp.h>
 
-/* STUB - clock_gettime is the only symbol that causes a dependency on 
+/* STUB - clock_gettime is the only symbol that causes a dependency on
  * GLIBC 2.17. I assume a high-resolution clock is not critical for a parser, so
  * this may just work.. */
 int clock_gettime(int clk_id, struct timespec *res) {
-    return 0;
-}
-
-int cortomain(int argc, char *argv[]) {
-
     return 0;
 }
